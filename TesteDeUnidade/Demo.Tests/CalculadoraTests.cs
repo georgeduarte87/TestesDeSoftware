3ce83a9ea848c23ea5eff43fc06bf04cc951f8f6
George Duarte <contato@georgeduarte.com.br>
2022-06-02 16:45:17 -0300

    Testes com Theory

--- TesteDeUnidade/Demo.Tests/CalculadoraTests.cs
+++ TesteDeUnidade/Demo.Tests/CalculadoraTests.cs
@@ -16,5 +16,24 @@ namespace Demo.Tests
             //Assert
             Assert.Equal(4, resultado);
         }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(4, 4, 8)]
+        [InlineData(8, 8, 16)]
+        [InlineData(3, 3, 6)]
+        [InlineData(6, 6, 12)]
+        public void Calculadora_Somar_RetornarValorSomaCorreta(double v1, double v2, double total)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total, resultado);
+        }
     }
 }
